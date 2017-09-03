542e55e9cedae4d08490fb6aac6abc9396b5e258
Rizky Ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2017-09-03 21:23:19 +0700

    new namespace

--- main.go
+++ main.go
@@ -7,8 +7,34 @@ import (
 	"syscall"
 )
 
-
+// go run main.go run <cmd> <args>
+// Same with docker run images <cmd> <args>
 func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		panic("help")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
+	must(cmd.Run())
+}
+
+func child() {
 	switch os.Args[1] {
 	case "run":
 		run()
@@ -24,9 +50,6 @@ func run() {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS, 
-	}
 
 	must(syscall.Sethostname([]byte("container")))
 
